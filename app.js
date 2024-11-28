29e462ee674443ff3ac9d61ac5a2c64b49248a2f
Amit John <amit.p@scalgo.net>
2024-11-28 11:40:08 +0530

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
     cors({
-        origin: ['http://localhost:7070'],
+        origin: ['http://localhost:7777'],
         credentials: true,
     })
 );
