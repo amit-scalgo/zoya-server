cb188bb22b9f919d969c0ba54d2f254d135f47f5
Amit John <amit.p@scalgo.net>
2024-11-28 13:11:37 +0530

    update

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,6 @@ import { verifyToken } from '../middlewares/auth.middleware.js';
 const userRoute = express.Router();
 
 userRoute.get('/profile', verifyToken, getUserDetail);
-userRoute.get('/users', verifyToken, getAllUsers);
+userRoute.get('/list', verifyToken, getAllUsers);
 
 export default userRoute;
