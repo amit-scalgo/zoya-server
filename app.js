f4780710078da1a2f01da655d86c941203e49496
Amit John <amit.p@scalgo.net>
2024-12-02 12:29:32 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { Server } from "socket.io";
-import { connectDB } from "./lib/db/connection.js";
-import authRoute from "./routes/auth.route.js";
-import userRoute from "./routes/user.route.js";
-import chatRoute from "./routes/chat.route.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import { connectDB } from './lib/db/connection.js';
+import authRoute from './routes/auth.route.js';
+import userRoute from './routes/user.route.js';
+import chatRoute from './routes/chat.route.js';
 
 dotenv.config();
 
@@ -13,10 +13,10 @@ const app = express();
 
 // Middleware
 app.use(
-  cors({
-    origin: ["http://localhost:7777", "https://zoya-web.vercel.app"],
-    credentials: true,
-  })
+    cors({
+        origin: ['http://localhost:7777', 'https://zoya-web.vercel.app'],
+        credentials: true,
+    })
 );
 
 app.use(express.json());
@@ -26,81 +26,87 @@ connectDB();
 
 // Routes
 // Authentication routes
-app.use("/api/auth", authRoute);
-app.use("/api/user", userRoute);
-app.use("/api/chat", chatRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
+app.use('/api/chat', chatRoute);
 
 const server = app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+    console.log(`Server running on port ${process.env.PORT}`)
 );
 
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:7777", "https://zoya-web.vercel.app"],
-    credentials: true,
-  },
+    cors: {
+        origin: [
+            'http://localhost:7777',
+            'https://zoya-web.vercel.app',
+            'https://zoya-client.vercel.app',
+        ],
+        credentials: true,
+    },
 });
 
 // ONLINE USERS
 global.onlineUsers = new Map();
 
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
-    console.log(`User added: ${userId}`);
-    console.log("Current online users:", Array.from(onlineUsers.keys()));
-    socket.emit("online-users", {
-      onlineUsers: Array.from(onlineUsers.keys()),
+io.on('connection', (socket) => {
+    global.chatSocket = socket;
+    socket.on('add-user', (userId) => {
+        onlineUsers.set(userId, socket.id);
+        console.log(`User added: ${userId}`);
+        console.log('Current online users:', Array.from(onlineUsers.keys()));
+        socket.emit('online-users', {
+            onlineUsers: Array.from(onlineUsers.keys()),
+        });
     });
-  });
-  socket.on("signout", (id) => {
-    onlineUsers.delete(id);
-    socket.emit("online-users", {
-      onlineUsers: Array.from(onlineUsers.keys()),
+    socket.on('signout', (id) => {
+        onlineUsers.delete(id);
+        socket.emit('online-users', {
+            onlineUsers: Array.from(onlineUsers.keys()),
+        });
     });
-  });
 
-  // Handle outgoing call
-  socket.on("outgoing-call", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("incoming-call", {
-        from: data.from,
-        signal: data.signal,
-      });
-    }
-  });
+    // Handle outgoing call
+    socket.on('outgoing-call', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('incoming-call', {
+                from: data.from,
+                signal: data.signal,
+            });
+        }
+    });
 
-  // Handle answer call
-  socket.on("answer-call", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("call-accepted", data.signal);
-    }
-  });
+    // Handle answer call
+    socket.on('answer-call', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('call-accepted', data.signal);
+        }
+    });
 
-  socket.on("reject-voice-call", (data) => {
-    const sendUserSocket = onlineUsers.get(data.from);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("voice-call-rejected");
-    }
-  });
+    socket.on('reject-voice-call', (data) => {
+        const sendUserSocket = onlineUsers.get(data.from);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('voice-call-rejected');
+        }
+    });
 
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", {
-        from: data.from,
-        message: data.message,
-      });
-    }
-  });
+    socket.on('send-msg', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-recieve', {
+                from: data.from,
+                message: data.message,
+            });
+        }
+    });
 
-  socket.on("mark-read", ({ id, recieverId }) => {
-    const sendUserSocket = onlineUsers.get(id);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("mark-read-recieve", { id, recieverId });
-    }
-  });
+    socket.on('mark-read', ({ id, recieverId }) => {
+        const sendUserSocket = onlineUsers.get(id);
+        if (sendUserSocket) {
+            socket
+                .to(sendUserSocket)
+                .emit('mark-read-recieve', { id, recieverId });
+        }
+    });
 });
