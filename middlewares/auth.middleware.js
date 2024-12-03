6c06d5db0adc618ac7b03defffb595320e00a138
Amit John <amit.p@scalgo.net>
2024-12-03 11:10:13 +0530

    update

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,20 +1,20 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
-  try {
-    const token =
-      req?.cookies?.token || req.headers.authorization.split(" ")[1];
+    try {
+        const token =
+            req?.cookies?.token || req.headers?.authorization?.split(' ')[1];
 
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized: No token provided" });
+        if (!token) {
+            return res
+                .status(401)
+                .json({ message: 'Unauthorized: No token provided' });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error('Error verifying token:', error.message);
+        res.status(403).json({ message: 'Invalid or expired token' });
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    console.error("Error verifying token:", error.message);
-    res.status(403).json({ message: "Invalid or expired token" });
-  }
 };
