3121624d1ba49ae0b582a1dd726493b6e9477494
Amit John <amit.p@scalgo.net>
2024-12-03 11:29:09 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -58,3 +58,23 @@ export const getContactList = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+// UPDATE USER PROFILE
+export const updateProfile = async (req, res) => {
+    const { name, email, phoneNumber, avatar } = req.body;
+    try {
+        const loggedInUserId = req.user.id;
+        const updatedUser = await User.findByIdAndUpdate(
+            loggedInUserId,
+            { name, email, phoneNumber, avatar },
+            { new: true }
+        ).select('-password');
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error('Error updating user profile:', error.message);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,7 @@ import {
     getContactList,
     getUserDetail,
     getUserDetailByid,
+    updateProfile,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../middlewares/auth.middleware.js';
 
@@ -10,6 +11,7 @@ const userRoute = express.Router();
 
 userRoute.get('/profile', verifyToken, getUserDetail);
 userRoute.get('/detail/:uid', verifyToken, getUserDetailByid);
+userRoute.patch('/profile', verifyToken, updateProfile);
 userRoute.get('/list', verifyToken, getContactList);
 
 export default userRoute;
